065104ff768d3556f115feaae9f769b22b0144a2
k2foxspb <k2foxpsb@mail.ru>
2023-06-21 22:18:37 +0300

    slug manually

--- exp/models.py
+++ exp/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from django.template.defaultfilters import slugify
+
 from django.urls import reverse
 
 
 class News(models.Model):
-    title = models.CharField(max_length=256, verbose_name="Title")
+    title = models.CharField(max_length=256, unique=True, verbose_name="Title")
     preamble = models.CharField(max_length=1024, verbose_name="Preamble")
     body = models.TextField(blank=True, null=True, verbose_name="Body")
     created = models.DateTimeField(auto_now_add=True, verbose_name="Created", editable=False)
