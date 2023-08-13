f64c48dd8e4a3f6873eec93c047c0725b527b951
k2foxspb <k2foxpsb@mail.ru>
2023-08-13 17:10:57 +0300

    kseniya

--- static/js/main.js
+++ static/js/main.js
@@ -101,3 +101,16 @@ function carousel(root) {
         }
     };
 }
+$(window).on('load pageshow', function () {
+    $('body').fadeIn();
+});
+$("a:not([href*=\\#])").click(function() {
+    if (!$(this).attr('target')) {
+        $('body').fadeOut();
+        let url = $(this).attr('href');
+        window.setTimeout(function() {
+            window.location.href = url;
+        }, 500);
+        return false;
+    }
+});
