948cc7349fb04e68761ab19cba561349debf0d63
k2foxspb <k2foxpsb@mail.ru>
2023-08-17 21:02:08 +0300

    tasks

--- authapp/tasks.py
+++ authapp/tasks.py
@@ -3,12 +3,12 @@ from celery import shared_task
 
 
 @shared_task()
-def send_feedback_email_task(email, username):
+def send_feedback_email_task(username):
     """Sends an email when the feedback form has been submitted."""
 
     send_mail(
         'привет от экспертов',
-        f'новый пользователь  {email}{username}',
+        f'новый пользователь  {username}',
         'sudexpert2023@mail.ru',
         ['k2foxspb@mail.ru'],
         fail_silently=False,
