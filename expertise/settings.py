cae686c7f06b6862a507a7771fe140afe0588a0c
Valeriy <k2foxspb@mail.ru>
2023-08-21 14:11:36 +0300

    TimeZone = True

--- expertise/settings.py
+++ expertise/settings.py
@@ -125,11 +125,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
-USE_TZ = False
+USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
