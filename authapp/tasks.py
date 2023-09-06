052355e83a4cb04b75df3970955150ad6b35c0e9
k2foxspb <k2foxpsb@mail.ru>
2023-09-06 10:33:37 +0300

    celery

--- authapp/tasks.py
+++ authapp/tasks.py
@@ -1,5 +1,5 @@
 from time import sleep
-from django.core.mail import send_mail
+from django.core.mail import send_mass_mail
 from celery import shared_task
 
 from expertise.settings import EMAIL_HOST_USER
@@ -9,12 +9,11 @@ from expertise.settings import EMAIL_HOST_USER
 def send_feedback_email_task(email, firs_name, last_name):
     """Sends an email when the feedback form has been submitted."""
     sleep(2)  # Simulate expensive operation(s) that freeze Django
-    send_mail(
+    send_mass_mail(
         "Your Feedback",
         f"\t{firs_name} {last_name}\n\nСпасибо за регистрацию!",
         EMAIL_HOST_USER,
         ['k2foxspb@mail.ru', 'k2foxspb1991@yandex.ru', email],
-        fail_silently=False,
     )
 
 
@@ -22,10 +21,10 @@ def send_feedback_email_task(email, firs_name, last_name):
 def send_feedback_email_task_update(email, firs_name, last_name):
     """Sends an email when the feedback form has been submitted."""
     sleep(2)  # Simulate expensive operation(s) that freeze Django
-    send_mail(
+    send_mass_mail(
         "Your Feedback",
         f"\t{firs_name} {last_name}\n\nВы изменили учётную запись!",
         EMAIL_HOST_USER,
         ['k2foxspb@mail.ru', 'k2foxspb1991@yandex.ru', email],
-        fail_silently=False,
+
     )
