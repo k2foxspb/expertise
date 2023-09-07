3a8eeba0e241088061656f936a68fc256c04be9a
k2foxspb <k2foxpsb@mail.ru>
2023-09-07 10:54:44 +0300

    forgot pass

--- authapp/urls.py
+++ authapp/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import PasswordResetConfirmView
+from django.contrib.auth.views import PasswordResetConfirmView, PasswordResetCompleteView
 from django.urls import path, include
 
 from authapp import views
@@ -15,7 +15,9 @@ urlpatterns = [
     path('password-reset-confirm/<uidb64>/<token>/',
          PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'),
          name='password_reset_confirm'),
-
+    path('password-reset-complete/',
+         PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
+         name='password_reset_complete'),
     path(
         "profile_edit/<int:pk>/",
         views.ProfileEditView.as_view(),
