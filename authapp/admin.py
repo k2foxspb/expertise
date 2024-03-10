0518f6c65003bc5d039f59f87eb3fe0fa9d7c8e5
k2foxspb <k2foxspb@mail.ru>
2024-03-10 06:45:24 +0300

    schema.org

--- authapp/admin.py
+++ authapp/admin.py
@@ -5,6 +5,7 @@ from authapp import models
 
 @admin.register(models.CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username', 'email', ]
     search_fields = ["id", "username", "email", "is_active", "date_joined"]
     ordering = ["-date_joined"]
     # list_filter = ('id', 'date_joined')
